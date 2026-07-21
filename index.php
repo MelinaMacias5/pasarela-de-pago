58ad8986cfc3f16a7d4e0e74c561e4edecd02b28
lizzardi <e1316892304@live.uleam.edu.ec>
2026-07-20 23:44:48 -0500

    chore: update page title to PetTrack Pro plus

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PetTrack Pro - Checkout</title>
+    <title>PetTrack Pro plus - Checkout</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
